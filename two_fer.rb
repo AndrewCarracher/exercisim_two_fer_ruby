c445d66215cc3585176f9312bf2b07d3a671fe1e
AndrewCarracher <andrew.carracher@hotmail.co.uk>
2018-09-15 12:33:52 +0100

    two-fer completed

---
+++ two_fer.rb
@@ -0,0 +1,12 @@
+class TwoFer
+  def self.two_fer(name = "")
+    case name
+    when "Alice"
+       "One for Alice, one for me."
+    when "Bob"
+       "One for Bob, one for me."
+    else
+       "One for you, one for me."
+    end
+  end
+end
